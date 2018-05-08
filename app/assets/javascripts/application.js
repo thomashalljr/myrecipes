a6a609e48f5ed279171270d63ccf68505f0009fb
Thomas Hall Jr <thomashalljunior@gmail.com>
2018-05-08 13:44:25 -0400

    Complete chefchat feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,36 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
+//= require jquery_ujs
+// require rails-ujs
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+function scrollToBottom() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+function submitMessage(event) {
+  event.preventDefault();
+  $('#new_message').submit();
+}
+
+$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+  if (event.keyCode === 13) {
+    submitMessage(event);
+  }
+});
+
+$(document).on('click', '[data-send~=message]', function(event) {
+  submitMessage(event);
+});
+
+$(document).on('turbolinks:load', function() {
+  $("#new_message").on("ajax:complete", function(e, data, status) {
+    $('#message_content').val('');
+  })
+  scrollToBottom();
+});
