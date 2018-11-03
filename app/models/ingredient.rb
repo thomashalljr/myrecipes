7b7c6d762240be71e45bd1fd51e3ac55032574e2
Thomas Hall Jr <thomashalljunior@gmail.com>
2018-11-03 15:55:43 -0400

    Alphabetize ingredients in drop down menu

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,13 @@
 class Ingredient < ApplicationRecord
+  before_validation :to_lower
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  private
+
+  def to_lower
+    self.name = self.name.downcase
+  end
 end
