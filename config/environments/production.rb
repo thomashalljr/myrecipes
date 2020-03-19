2c9659861501094497cdfa63cc64d41cbdfc63e1
Thomas Hall Jr <thomashalljunior@gmail.com>
2020-03-18 21:50:02 -0400

    Adjust SendGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,9 +95,22 @@ config.webpacker.check_yarn_integrity = false
   config.action_cable.allowed_request_origins = ['https://enjoymyrecipes.herokuapp.com', 'http://enjoymyrecipes.herokuapp.com']
 
   # SendGrid config on Heroku
-  config.action_mailer.default_url_options = { :host => 'enjoymyrecipes.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.default_url_options = { :host => 'enjoymyrecipes.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
+
+  # Setup the mailer config
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.smtp_settings = {
+   :user_name => ENV['SENDGRID_USERNAME'],
+   :password => ENV['SENDGRID_PASSWORD'],
+   :domain => 'https://enjoymyrecipes.herokuapp.com/',
+   :address => 'smtp.sendgrid.net',
+   :port => 587,
+   :authentication => :plain,
+   :enable_starttls_auto => true
+ }
 end
