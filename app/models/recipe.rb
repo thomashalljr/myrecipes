4b2c4727a420bd1def2f65f013ac2d4272ae7374
Thomas Hall Jr <thomashalljunior@gmail.com>
2020-10-17 17:52:12 -0400

    Remove length validation on recipe description

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
-  validates :description, presence: true, length: {minimum: 5, maximum: 750}
+  validates :description, presence: true, length: {minimum: 5}
   belongs_to :chef
   validates :chef_id, presence: true
   default_scope -> { order(updated_at: :desc) }
