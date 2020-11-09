cf95b4359bb97567f67762e3720d85b3de0844c2
Thomas Hall Jr <thomashalljunior@gmail.com>
2020-11-08 20:46:28 -0500

    Remove example React component

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,4 @@
 class Chef < ApplicationRecord
-
   before_save {self.email = email.downcase}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -16,8 +15,7 @@ class Chef < ApplicationRecord
 
   validates :chefname, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
 end
