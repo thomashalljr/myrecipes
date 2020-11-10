d17245bb75ca4182a70681df9d338cb1fb4ac93a
Thomas Hall Jr <thomashalljunior@gmail.com>
2020-11-10 15:39:33 -0500

    Fix bug

--- app/controllers/chefs/sessions_controller.rb
+++ app/controllers/chefs/sessions_controller.rb
@@ -1,11 +1,11 @@
 class Chefs::SessionsController < Devise::SessionsController
   def create
-    cookies.encrypted[:chef_id] = chef_id
+    cookies.encrypted[:chef_id] = chef_id if chef_id
     super
   end
 
   private
     def chef_id
-      Chef.select(:id).where("email = ?", params[:chef][:email]).first.id
+      Chef.select(:id).where("email = ?", params[:chef][:email]).first.try(:id)
     end
 end
